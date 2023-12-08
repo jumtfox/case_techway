74e470cf48ce9ce94bfe39dbca8030926b5ba675
jumtfox <julianopereira@gmail.com>
2023-12-08 13:40:19 -0300

    update

--- page_app/urls.py
+++ page_app/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from page_app.views import index, contato
+from page_app.views import index, contato, services
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Cadastrar URLs aqui
     path('', index),
-
+    
     path('contato/', contato),
+
+    path('services/', services),
+ 
 ]
+
+
+
